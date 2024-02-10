8f9090f5f52d3b1255a8fb0d86b767ae059de631
cutplane1 <snakesnake222@mail.ru>
2024-02-10 19:34:05 +0500

    suits

---
+++ src/internal/Suit.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Suit(Enum):
+    Clubs = 0
+    Spades = 1
+    Hearts = 2
+    Diamonds = 3
--- src/raylib_ex.py
+++ src/raylib_ex.py
@@ -1,38 +1,58 @@
 from pyray import *
+import random
+from internal import Suit 
 
 init_window(800, 600, 'demo')
 
 set_target_fps(60)
 
 class draw:
-    def __enter__(self):
+    def __enter__(self) -> None:
         begin_drawing()
     
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         end_drawing()
 
 
 class Card:
-    def __init__(self, suit, rank, x, y) -> None:
+    def __init__(self, suit: Suit, rank: int, x: int, y: int) -> None:
         self.suit = suit
-        self.rank = rank
+        self.rank = str(rank)
         self.x = x
         self.y = y
-
+        
+        if suit.name is "Clubs" or suit.name is "Spades":
+            self.color = BLACK
+        else:
+            self.color = RED
 
     def draw(self) -> None:
-        draw_rectangle(self.x, self.y,50,70,RED)
+        draw_rectangle(self.x, self.y, 50, 70, WHITE)
+        draw_text(self.rank, self.x + 2, self.y + 2, 20, self.color)
 
-c = Card("bc", 8, 5, 5)
 
+cards = []
+
+# for i in range(1, 9 + 1):
+#     cards.append(Card("lol", str(i), i * 10 * 6 + 50, 10))
+
+# card spawn
+
+for i in range(1, 9 + 1):
+    cards.append(Card(random.choice(list(Suit)), i, i * 10 * 6 + 50, 500))
 
 while not window_should_close():
-    c.x += 2
 
     with draw():
-        clear_background(RAYWHITE)
-        c.draw()
+        clear_background(BLACK)
 
+        draw_rectangle(100, 10, 600, 100, BROWN)
+        draw_text("enemy deck", 350, 50, 24, WHITE)
 
+        draw_circle(500, 300, 60, GRAY)
+        draw_text("card pack", 460, 290, 16, WHITE)
+        
+        for c in cards:
+            c.draw()
 
 close_window()
