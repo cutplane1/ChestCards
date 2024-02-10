baa9bdcc42076fe61d356ed2d7ed7382824d36df
cutplane1 <snakesnake222@mail.ru>
2024-02-10 16:26:57 +0500

    rect

--- src/main.py
+++ src/main.py
@@ -4,13 +4,18 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 240))
 
-running = True
-while running:
+r = pygame.Rect(0, 0, 20, 20)
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEMOTION:
             print(event.pos)
 
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            quit()
 
-pygame.quit()
+
+# class Card:
+#     def __init__(self, suit, rank, x, y) -> None:
+        
