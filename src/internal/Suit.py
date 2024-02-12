48a6045ebbf20bacf7958d7d176b19d5a69fd747
cutplane1 <snakesnake222@mail.ru>
2024-02-12 19:14:06 +0500

    deck

--- src/internal/Suit.py
+++ src/internal/Suit.py
@@ -1,7 +1,11 @@
 from enum import Enum
+from random import choice
 
 class Suit(Enum):
     Clubs = 0
     Spades = 1
     Hearts = 2
     Diamonds = 3
+
+    def random():
+        return choice(list(Suit))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from pyray import *
-import random
+from raylib import colors
 from internal import Suit 
 
 init_window(800, 600, 'demo')
@@ -22,14 +22,14 @@ class Card:
         self.texture = texture
         
         if suit.name == "Clubs" or suit.name == "Spades":
-            self.color = BLACK
+            self.color = colors.BLACK
         elif suit.name == "Hearts" or suit.name == "Diamonds":
-            self.color = RED
+            self.color = colors.RED
 
     def draw(self) -> None:
         draw_rectangle(self.x, self.y, 50, 70, WHITE)
         draw_text(self.rank, self.x + 2, self.y + 2, 20, self.color)
-        draw_texture(self.texture, self.x + 30, self.y + 50, WHITE)
+        draw_texture(self.texture, self.x + 30, self.y + 50, colors.WHITE)
 
 class CardFactory:
     def __init__(self) -> None:
@@ -45,33 +45,65 @@ class CardFactory:
 
 
 class Deck:
-    def __init__(self) -> None:
+    def __init__(self, factory: CardFactory) -> None:
+        self.factory = factory
         self.cells = {}
     
     def place_card(card: Card) -> None:
         pass
-    # def in_this_cell(cell: int, card: Card) -> None:
+
+    def in_this_cell(cell: int, card: Card) -> None:
+        # factory.spawn_card()
+        pass
+
+    def debug_init(self, cell: int):
+        try:
+            self.cells[cell].append(self.factory.spawn_card(Suit.random(), 7, 100, 100))
+        except KeyError:
+            self.cells[cell] = [self.factory.spawn_card(Suit.random(), 7, 100, 100)]
+
+
+
 
 
 # card spawn
-cards = []
+# cards = []
 factory = CardFactory()
 
-for i in range(2, 9 + 1):
-    cards.append(factory.spawn_card(random.choice(list(Suit)), i, i * 10 * 6 + 50, 500))
+deck = Deck(factory)
+deck.debug_init(1)
+deck.debug_init(1)
+deck.debug_init(1)
+# try:
+#     print(deck.cells[1])
+# except KeyError:
+#     deck.cells[1] = []
+
+print(deck.cells)
+
+
+# deck.in_this_cell(1, factory.spawn_card(random.choice(list(Suit)), i, i * 10 * 6 + 50, 500)))
+
+# for i in range(2, 9 + 1):
+    # cards.append(factory.spawn_card(random.choice(list(Suit)), i, i * 10 * 6 + 50, 500))
 
 while not window_should_close():
 
     with draw():
         clear_background(BLACK)
 
-        draw_rectangle(100, 10, 600, 100, BROWN)
-        draw_text("enemy deck", 350, 50, 24, WHITE)
+        # draw_rectangle(100, 10, 600, 100, colors.BROWN)
+        draw_text("enemy side", 0, 0, 24, colors.WHITE)
+        for d in deck.cells:
+            for i in deck.cells[d]:
+                i.draw()
 
-        draw_circle(500, 300, 60, GRAY)
-        draw_text("card pack", 460, 290, 16, WHITE)
+
+
+        # draw_circle(500, 300, 60, GRAY)
+        # draw_text("card pack", 460, 290, 16, colors.WHITE)
         
-        for c in cards:
-            c.draw()
+        # for c in cards:
+        #     c.draw()
 
 close_window()
