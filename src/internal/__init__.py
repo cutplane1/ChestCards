0d62d5707abcf27552f9554769a212e28644a5f9
cutplane1 <snakesnake222@mail.ru>
2024-02-15 21:57:57 +0500

    ioerror

--- src/internal/__init__.py
+++ src/internal/__init__.py
@@ -2,6 +2,5 @@ from .helpers import *
 import pyray
 from .Suit import Suit
 from .Card import Card
-from .TextureOpenFailedException import TextureOpenFailedException
 from .CardFactory import CardFactory
 from .Deck import Deck
